a9b2b3c4a9244beaba7cd0f98c2eba361540b3c6
TuhuTime <louistardinghen@gmail.com>
2026-01-27 16:43:14 +0100

    modified:   src/custom_math.c

--- src/custom_math.c
+++ src/custom_math.c
@@ -1,4 +1,7 @@
 #include "custom_math.h"
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
 
 double taylor_sin(double x)
 {
@@ -17,4 +20,14 @@ double taylor_sin(double x)
     }
 
     return result;
+}
+
+float generate_sample(int type, float phase) {
+    switch (type) {
+        case 0: return sinf(2.0f * PI * phase);
+        case 1: return (sinf(2.0f * PI * phase) >= 0) ? 1.0f : -1.0f;
+        case 2: return 2.0f * fabsf(2.0f * (phase - floorf(phase + 0.5f))) - 1.0f;
+        case 3: return 2.0f * (phase - floorf(phase + 0.5f));
+        default: return 0.0f;
+    }
 }
