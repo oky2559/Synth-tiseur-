dabd543b99541fd97b3c991226ca26b25c0bf3b8
oky2559 <schmutz.vict@gmail.com>
2026-01-31 18:22:56 +0100

    Update custom_math.c

--- src/custom_math.c
+++ src/custom_math.c
@@ -3,13 +3,14 @@
 #include <string.h>
 #include <stdlib.h>
 
+
 float generate_sample(int type, float phase) {
     switch (type) {
         case 0: return sinf(2.0f * PI * phase); // onde sinusoïdale
         case 1: return (sinf(2.0f * PI * phase) >= 0) ? 1.0f : -1.0f; // onde carrée
-        case 2: return 2.0f * fabsf(2.0f * (phase - floorf(phase + 0.5f))) - 1.0f; // onde en dents de scie
-        case 3: return 2.0f * (phase - floorf(phase + 0.5f)); // onde triangulaire
-        default: return 0.0f; // silence
+        case 2: return 2.0f * (phase - floorf(phase + 0.5f)); // onde en dents de scie 
+        case 3: return 2.0f * fabsf(2.0f * (phase - floorf(phase + 0.5f))) - 1.0f; // onde triangulaire
+        default: return 0.0f;
     }
 }
 
